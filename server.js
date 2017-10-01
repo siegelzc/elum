ee598966f3967b547a7f54a0b0856ad7ca77db23
siegelzc <siegelzc@gmail.com>
2017-09-30 19:31:32 -0500

    server-reset

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 // io.sockets.emit('Saves', saves); // Emit to all clients
 
 // Express
-var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+// var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var express = require('express');
 var app = express();
-var server = app.listen(port, ip);
+var server = app.listen(8080);
 
 // Socket.io
 var socketio = require('socket.io');
--- www/elum/js/sketch.js
+++ www/elum/js/sketch.js
@@ -112,7 +112,7 @@ var load = {
 function setup(neW) {
 	if (neW != false) {
 		// Sockets
-		// socket = io.connect('localhost:80'); // Local
+		// socket = io.connect('localhost:8080'); // Local
 		socket = io.connect('http://node-elum.1d35.starter-us-east-1.openshiftapps.com:8080'); // Openshift
 		socket.emit('GalaxyLoaded');
 		socket.on('GalaxyLoaded', function(loadeD) {
